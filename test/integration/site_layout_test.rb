039576825a8481432c7b6e9db36b4565febae38d
dkuji <daisuke.kuji@gmail.com>
2019-02-11 01:46:56 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,16 +1,47 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", '/login'
+    assert_select "a[href=?]", '/signup'
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", 'http://news.railstutorial.org/'
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    log_in_as(@user)
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", user_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", 'http://news.railstutorial.org/'
   end
+
+  #test "layout links lgged in" do
+  #  #assert_select "a[href=?]", root_path#, count: 2
+  #  assert_select "a[href=?]", help_path
+  #  assert_select "a[href=?]", users_path
+  #  assert_select "a[href=?]", user_path(user)
+  #  assert_select "a[href=?]", about_path
+  #  assert_select "a[href=?]", contact_path
+  #  assert_select "a[href=?]", 'http://news.railstutorial.org/'
+  #end
 end
