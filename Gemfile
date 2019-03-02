a70af67b0d6f48b785c2f6e36ac9869ba88e321e
dkuji <daisuke.kuji@gmail.com>
2019-03-03 00:53:59 +0900

    Add account activation

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'bootstrap-sass', '3.3.7'
+gem 'dotenv-rails', '2.7.1', groups: [:development, :test]
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -17,6 +17,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
+        assert user.activated?
       end
     end
     assert_difference 'User.count', -1 do
